467c4d3063dbdd74cfd06bfc5fe2b6c3cbfef335
v-king777 <v.kolpashchikov@gmail.com>
2021-11-23 20:59:08 +0300

    23.11.21.20.59

---
+++ SkyNet/Controllers/Account/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkyNet.ViewModels.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkyNet.Controllers.Account
+{
+    public class RegisterController : Controller
+    {
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+    }
+}

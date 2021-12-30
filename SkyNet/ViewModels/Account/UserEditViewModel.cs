b7277f06b4911a26c63cc67b71a357f383f2dc2e
v-king777 <v.kolpashchikov@gmail.com>
2021-12-30 18:48:29 +0300

    30.12.21.18.48

--- SkyNet/ViewModels/Account/UserEditViewModel.cs
+++ SkyNet/ViewModels/Account/UserEditViewModel.cs
@@ -5,31 +5,36 @@ namespace SkyNet.ViewModels.Account;
 
 public class UserEditViewModel
 {
-    [Required]
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
     [Display(Name = "Идентификатор пользователя")]
     public string UserId { get; set; }
 
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
     [DataType(DataType.Text)]
     [Display(Name = "Имя", Prompt = "Введите имя")]
     public string FirstName { get; set; }
 
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
     [DataType(DataType.Text)]
     [Display(Name = "Фамилия", Prompt = "Введите фамилию")]
     public string LastName { get; set; }
 
-    [EmailAddress]
-    [Display(Name = "Email", Prompt = "example.com")]
-    public string Email { get; set; }
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
+    [DataType(DataType.Text)]
+    [Display(Name = "Отчество", Prompt = "Введите отчество")]
+    public string MiddleName { get; set; }
 
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
     [DataType(DataType.Date)]
     [Display(Name = "Дата рождения")]
     public DateTime BirthDate { get; set; }
 
-    public string UserName => Email;
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
+    [EmailAddress]
+    [Display(Name = "Email", Prompt = "example.com")]
+    public string Email { get; set; }
 
-    [DataType(DataType.Text)]
-    [Display(Name = "Отчество", Prompt = "Введите отчество")]
-    public string MiddleName { get; set; }
+    public string UserName => Email;
 
     [DataType(DataType.ImageUrl)]
     [Display(Name = "Фото", Prompt = "Укажите ссылку на картинку")]
